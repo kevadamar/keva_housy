d4af5566ed612ed796cf61923f80abc3416e581c
keva damar galih <kevadamarg@gmail.com>
2021-07-15 09:03:09 +0700

    fix context

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import './index.css';
 import App from './App';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { UserContextProvider } from './contexts/UserContext';
 import { SearchContextProvider } from './contexts/SearchContext';
-import { BookingContextProvider } from './contexts/BookingContext';
-import { ReactQueryDevtools } from 'react-query/devtools';
 
 const client = new QueryClient();
 
@@ -17,9 +17,7 @@ ReactDOM.render(
     <QueryClientProvider client={client}>
       <UserContextProvider>
         <SearchContextProvider>
-          <BookingContextProvider>
-            <App />
-          </BookingContextProvider>
+          <App />
         </SearchContextProvider>
       </UserContextProvider>
       <ReactQueryDevtools />
